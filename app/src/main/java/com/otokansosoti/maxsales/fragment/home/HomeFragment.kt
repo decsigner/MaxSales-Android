4dfa2bedaef1f9922f6e8b66a47be73e3ff35698
FMiciano <fabio.fferreira@mercadolivre.com>
2023-03-01 16:31:15 -0300

    adiciona botoes de help

--- app/src/main/java/com/otokansosoti/maxsales/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/otokansosoti/maxsales/fragment/home/HomeFragment.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -15,7 +17,6 @@ import com.otokansosoti.maxsales.databinding.FragmentHomeBinding
 import com.otokansosoti.maxsales.fragment.home.adapter.HomeAdapter
 import com.otokansosoti.maxsales.fragment.home.viewHolder.product.ProductViewModel
 import com.otokansosoti.maxsales.fragment.home.viewHolder.product.ProductViewModelFactory
-//import com.otokansosoti.maxsales.webview.WebViewActivity
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
@@ -51,6 +52,23 @@ class HomeFragment : Fragment() {
         viewModel.setupHomeList().observe(this.viewLifecycleOwner) { setupHomeList(it) }
     }
 
+    private fun configureActions() {
+        val aboutButton: ImageButton = binding.aboutButton
+        aboutButton.setOnClickListener {
+            showAbout()
+        }
+
+        val salesButton: ImageButton = binding.salesButton
+        salesButton.setOnClickListener {
+            showSales()
+        }
+
+        val sacButton: ImageButton = binding.sacButton
+        sacButton.setOnClickListener {
+            showSac()
+        }
+    }
+
     private fun setupHomeList(dataSource: List<HomeModel>) {
         val recyclerView = binding.recyclerView
         val adapter = HomeAdapter(dataSource, this.viewLifecycleOwner, this.requireContext())
@@ -61,21 +79,17 @@ class HomeFragment : Fragment() {
             override fun onItemClick(position: Int) {
                 var model = dataSource[position]
                 if (model.type == "product" && model.available) {
-                    showWebView(model.link_action)
+                    showProductList()
                 } else if (model.type == "product" && !model.available) {
                     Toast.makeText(this@HomeFragment.context, "Seu plano não está liberado para este serviço.", Toast.LENGTH_SHORT).show()
                 }
             }
         })
-
-        adapter.openSac().observe(this.viewLifecycleOwner) { showSac() }
-        adapter.openAbout().observe(this.viewLifecycleOwner) { showAbout() }
     }
 
-    private fun showWebView(url: String) {
-//        val intent = Intent(this.requireContext(), WebViewActivity::class.java)
-//        intent.putExtra("XUrl", url)
-//        startActivity(intent)
+
+    private fun showProductList() {
+
     }
 
     private fun showAbout() {
@@ -83,8 +97,15 @@ class HomeFragment : Fragment() {
 //        startActivity(intent)
     }
 
+    private fun showSales() {
+        val whatsUrl = "https://api.whatsapp.com/send?phone=5511995066617"
+        val whatsIntent = Intent(Intent.ACTION_VIEW)
+        whatsIntent.setData(Uri.parse(whatsUrl))
+        startActivity(whatsIntent)
+    }
+
     private fun showSac() {
-        val whatsUrl = "https://api.whatsapp.com/send?phone=5511937414931"
+        val whatsUrl = "https://api.whatsapp.com/send?phone=5511939466858"
         val whatsIntent = Intent(Intent.ACTION_VIEW)
         whatsIntent.setData(Uri.parse(whatsUrl))
         startActivity(whatsIntent)
